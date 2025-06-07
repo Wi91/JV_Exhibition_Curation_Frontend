5564ce18698f09b2522bfc2987646e97009e1f2d
WI91 <elammar.will@gmail.com>
2025-06-07 23:40:16 +0100

    Feat: Add exhibition list class

---
+++ app/src/main/java/com/northcoders/jv_exhibition_curation/ui/ExhibitionListFragment.java
@@ -0,0 +1,32 @@
+package com.northcoders.jv_exhibition_curation.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.northcoders.jv_exhibition_curation.R;
+
+
+public class ExhibitionListFragment extends Fragment {
+
+
+    public ExhibitionListFragment() {}
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_exhibition_list, container, false);
+    }
+}
