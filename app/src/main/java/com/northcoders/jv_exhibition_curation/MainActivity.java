208593821c27fd3b8ccef17e4a4bd5914e8f2a93
WI91 <elammar.will@gmail.com>
2025-06-07 23:39:40 +0100

    Feat: Add logic to open exhibitions when selected on nav bar

--- app/src/main/java/com/northcoders/jv_exhibition_curation/MainActivity.java
+++ app/src/main/java/com/northcoders/jv_exhibition_curation/MainActivity.java
@@ -1,5 +1,6 @@
 package com.northcoders.jv_exhibition_curation;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -7,6 +8,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.navigation.NavigationBarView;
+import com.northcoders.jv_exhibition_curation.ui.ExhibitionListFragment;
 import com.northcoders.jv_exhibition_curation.ui.HomepageFragment;
 import com.northcoders.jv_exhibition_curation.ui.SearchArtworks;
 
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
     HomepageFragment homepageFragment = new HomepageFragment();
 
     SearchArtworks searchArtworks = new SearchArtworks();
+
+    ExhibitionListFragment exhibitionListFragment = new ExhibitionListFragment();
 
 
     @Override
@@ -46,9 +50,18 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
                     .beginTransaction()
                     .replace(R.id.baseFragment, searchArtworks)
                     .commit();
+            return true;
+        }
+
+        if (item.getItemId() == R.id.exhibitions) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.baseFragment, exhibitionListFragment)
+                    .commit();
+
         }
         return true;
-}
+    }
 }
 
 
