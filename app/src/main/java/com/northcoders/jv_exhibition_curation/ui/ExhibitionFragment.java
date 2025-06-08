f0630014a226737fffd1afb4852c845f1be9fcb4
WI91 <elammar.will@gmail.com>
2025-06-08 01:36:05 +0100

    Feat: Refactor exhibition fragment class

--- app/src/main/java/com/northcoders/jv_exhibition_curation/ui/ExhibitionFragment.java
+++ app/src/main/java/com/northcoders/jv_exhibition_curation/ui/ExhibitionFragment.java
@@ -1,5 +1,29 @@
 package com.northcoders.jv_exhibition_curation.ui;
 
-public class ExhibitionFragment {
+import android.os.Bundle;
 
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.northcoders.jv_exhibition_curation.R;
+
+public class ExhibitionFragment extends Fragment {
+
+    public ExhibitionFragment() {}
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_exhibition, container, false);
+    }
 }
