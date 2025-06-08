53aa2a3daa6a025463f3b0ad5041ee6382fbfc26
WI91 <elammar.will@gmail.com>
2025-06-08 01:36:28 +0100

    Feat: Add artwork model

---
+++ app/src/main/java/com/northcoders/jv_exhibition_curation/model/Artwork.java
@@ -0,0 +1,13 @@
+package com.northcoders.jv_exhibition_curation.model;
+
+public class Artwork {
+    Long id;
+    Long apiId;
+    String title;
+    String description;
+    String altText;
+    String artistName;
+    String imageUrl;
+    String apiOrigin;
+}
+
