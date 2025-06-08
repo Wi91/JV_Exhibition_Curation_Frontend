c2deb84941a3e87f780101d45f34cc8f6eae70d7
WI91 <elammar.will@gmail.com>
2025-06-08 03:37:42 +0100

    Feat: Add constuctor/getter to model

--- app/src/main/java/com/northcoders/jv_exhibition_curation/model/Artwork.java
+++ app/src/main/java/com/northcoders/jv_exhibition_curation/model/Artwork.java
@@ -9,5 +9,48 @@ public class Artwork {
     String artistName;
     String imageUrl;
     String apiOrigin;
+
+    public Artwork(Long id, Long apiId, String title, String description, String altText, String artistName, String imageUrl, String apiOrigin) {
+        this.id = id;
+        this.apiId = apiId;
+        this.title = title;
+        this.description = description;
+        this.altText = altText;
+        this.artistName = artistName;
+        this.imageUrl = imageUrl;
+        this.apiOrigin = apiOrigin;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getApiId() {
+        return apiId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAltText() {
+        return altText;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getApiOrigin() {
+        return apiOrigin;
+    }
 }
 
--- app/src/main/java/com/northcoders/jv_exhibition_curation/model/Exhibition.java
+++ app/src/main/java/com/northcoders/jv_exhibition_curation/model/Exhibition.java
@@ -5,4 +5,16 @@ public class Exhibition {
     private Long id;
     private String title;
 
+    public Exhibition(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
