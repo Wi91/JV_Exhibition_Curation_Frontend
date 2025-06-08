d95341f6f4c497844cd8d433c12e2af3bb7c9f21
WI91 <elammar.will@gmail.com>
2025-06-08 18:03:09 +0100

    Feat: implement parcelable on class

--- app/src/main/java/com/northcoders/jv_exhibition_curation/model/Artwork.java
+++ app/src/main/java/com/northcoders/jv_exhibition_curation/model/Artwork.java
@@ -1,6 +1,9 @@
 package com.northcoders.jv_exhibition_curation.model;
 
-public class Artwork {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Artwork implements Parcelable{
     Long id;
     Long apiId;
     String title;
@@ -20,6 +23,64 @@ public class Artwork {
         this.imageUrl = imageUrl;
         this.apiOrigin = apiOrigin;
     }
+
+    protected Artwork(Parcel in) {
+        if (in.readByte() == 0) {
+            id = null;
+        } else {
+            id = in.readLong();
+        }
+        if (in.readByte() == 0) {
+            apiId = null;
+        } else {
+            apiId = in.readLong();
+        }
+        title = in.readString();
+        description = in.readString();
+        altText = in.readString();
+        artistName = in.readString();
+        imageUrl = in.readString();
+        apiOrigin = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        if (id == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeLong(id);
+        }
+        if (apiId == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeLong(apiId);
+        }
+        dest.writeString(title);
+        dest.writeString(description);
+        dest.writeString(altText);
+        dest.writeString(artistName);
+        dest.writeString(imageUrl);
+        dest.writeString(apiOrigin);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Artwork> CREATOR = new Creator<Artwork>() {
+        @Override
+        public Artwork createFromParcel(Parcel in) {
+            return new Artwork(in);
+        }
+
+        @Override
+        public Artwork[] newArray(int size) {
+            return new Artwork[size];
+        }
+    };
 
     public Long getId() {
         return id;
