6e689dce2eb36d768fcd27906347f097a0ea3f3a
WI91 <elammar.will@gmail.com>
2025-06-09 00:55:30 +0100

    Feat: implement parcelable on model

--- app/src/main/java/com/northcoders/jv_exhibition_curation/model/Exhibition.java
+++ app/src/main/java/com/northcoders/jv_exhibition_curation/model/Exhibition.java
@@ -1,6 +1,11 @@
 package com.northcoders.jv_exhibition_curation.model;
 
-public class Exhibition {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class Exhibition implements Parcelable {
 
     private Long id;
     private String title;
@@ -10,11 +15,48 @@ public class Exhibition {
         this.title = title;
     }
 
+    protected Exhibition(Parcel in) {
+        if (in.readByte() == 0) {
+            id = null;
+        } else {
+            id = in.readLong();
+        }
+        title = in.readString();
+    }
+
+    public static final Creator<Exhibition> CREATOR = new Creator<Exhibition>() {
+        @Override
+        public Exhibition createFromParcel(Parcel in) {
+            return new Exhibition(in);
+        }
+
+        @Override
+        public Exhibition[] newArray(int size) {
+            return new Exhibition[size];
+        }
+    };
+
     public Long getId() {
         return id;
     }
 
     public String getTitle() {
         return title;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int i) {
+        if (id == null) {
+            parcel.writeByte((byte) 0);
+        } else {
+            parcel.writeByte((byte) 1);
+            parcel.writeLong(id);
+        }
+        parcel.writeString(title);
     }
 }
