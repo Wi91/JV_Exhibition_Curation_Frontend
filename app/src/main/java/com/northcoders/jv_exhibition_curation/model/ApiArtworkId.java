64ad9448df6ed97bd80d9fdf9b69bf4ced548eb6
WI91 <elammar.will@gmail.com>
2025-06-09 04:58:59 +0100

    Feat: make apiArtworkId class parcelable

--- app/src/main/java/com/northcoders/jv_exhibition_curation/model/ApiArtworkId.java
+++ app/src/main/java/com/northcoders/jv_exhibition_curation/model/ApiArtworkId.java
@@ -1,8 +1,62 @@
 package com.northcoders.jv_exhibition_curation.model;
 
-public class ApiArtworkId {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ApiArtworkId implements Parcelable {
 
     Long artworkId;
 
     String apiOrigin;
+
+    public ApiArtworkId(Long artworkId, String apiOrigin) {
+        this.artworkId = artworkId;
+        this.apiOrigin = apiOrigin;
+
+    }
+
+    protected ApiArtworkId(Parcel in) {
+        if (in.readByte() == 0) {
+            artworkId = null;
+        } else {
+            artworkId = in.readLong();
+        }
+        apiOrigin = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        if (artworkId == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeLong(artworkId);
+        }
+        dest.writeString(apiOrigin);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<ApiArtworkId> CREATOR = new Creator<ApiArtworkId>() {
+        @Override
+        public ApiArtworkId createFromParcel(Parcel in) {
+            return new ApiArtworkId(in);
+        }
+
+        @Override
+        public ApiArtworkId[] newArray(int size) {
+            return new ApiArtworkId[size];
+        }
+    };
+
+    public Long getArtworkId() {
+        return artworkId;
+    }
+
+    public String getApiOrigin() {
+        return apiOrigin;
+    }
 }
